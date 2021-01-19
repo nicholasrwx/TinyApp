86f72bcc6dd70756dc327fe354a1d5b001a8604d
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-01-18 23:24:07 -0700

    Added EJS template

--- express_server.js
+++ express_server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+//this route handler sends our url database to our EJS template
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -18,11 +23,10 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
