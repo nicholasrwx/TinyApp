50166649b20579ebce4ca224f6ea9e491ea729cd
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-01-20 04:15:23 -0700

    Added url Delete Button

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const generateRandomString = function () {
-return Math.random().toString(36).substr(2, 6); 
+  return Math.random().toString(36).substr(2, 6);
 };
 
 const urlDatabase = {
@@ -22,11 +22,13 @@ const urlDatabase = {
 //therefore you will never get to your other pages
 //rule of thumb: order from most basic routes, to least specific routes
 
+//index page 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//generate random tiny URL and redirect to urls show
 app.post("/urls", (req, res) => {
   const newurl = generateRandomString();
   urlDatabase[newurl] = req.body.longURL;
@@ -34,34 +36,52 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newurl}`);
 });
 
+//create new url page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//delete urls in database and req.params
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const key = req.params.shortURL;
+  console.log(urlDatabase);
+  delete urlDatabase[key];
+  delete req.params.shortURL;
+  console.log(req.params.shortURL);
+  console.log(urlDatabase);
+  res.redirect("/urls");
+});
+
+//displays newly created shortURL in a clickable link format
 app.get("/urls/:shortURL", (req, res) => {
-  const u = 'urls'
+  const u = "urls";
   const key = req.params.shortURL;
   const templateVars = { shortURL: key, longURL: urlDatabase[key], urls: u }; //urlDatabase?u
   res.render("urls_show", templateVars);
 });
 
+//makes the clickable shortURL link even shorter, and redirects to the associated long url page.
 app.get("/u/:shortURL", (req, res) => {
-const longURL = urlDatabase[req.params.shortURL];
-res.redirect(longURL)
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
+//prints hello in browser
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//prints urlDatabase in json format in the browser
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//inline html string sent that the browser will render upon receiving this response
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//allows client server interaction on specified port 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
