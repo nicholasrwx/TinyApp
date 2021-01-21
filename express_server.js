331cb69734dfdcfa520a59675d770b5ba841f901
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-01-20 17:24:40 -0700

    Add Edit Button, and URL Update Form

--- express_server.js
+++ express_server.js
@@ -1,58 +1,47 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const app = express();
+const express = require("express"); //Use Express
+const bodyParser = require("body-parser"); //Convert Buffer info into a usable txt format
+const app = express(); //express JS function redefined in variable form.
 const PORT = 8080; // default port 8080
+
+
 
 app.set("view engine", "ejs");
 
+//use bodyParser for everyhting?
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//allows client server interaction on specified port
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+//random string generator
 const generateRandomString = function () {
   return Math.random().toString(36).substr(2, 6);
 };
 
+//urlDatabase
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
 
-//this route handler sends our url database to our EJS template
-//the order of router definitions matter
-//they need to be above the :shortURL ... this one will look up the url always
-//therefore you will never get to your other pages
-//rule of thumb: order from most basic routes, to least specific routes
 
-//index page 
+//GET REQUESTS
+
+//GET URLS INDEX page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
-//generate random tiny URL and redirect to urls show
-app.post("/urls", (req, res) => {
-  const newurl = generateRandomString();
-  urlDatabase[newurl] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls/${newurl}`);
-});
 
-//create new url page
+//GET NEW URL page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-//delete urls in database and req.params
-app.post("/urls/:shortURL/delete", (req, res) => {
-  const key = req.params.shortURL;
-  console.log(urlDatabase);
-  delete urlDatabase[key];
-  delete req.params.shortURL;
-  console.log(req.params.shortURL);
-  console.log(urlDatabase);
-  res.redirect("/urls");
-});
-
-//displays newly created shortURL in a clickable link format
+//GET URL SHOW page
 app.get("/urls/:shortURL", (req, res) => {
   const u = "urls";
   const key = req.params.shortURL;
@@ -60,28 +49,52 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-//makes the clickable shortURL link even shorter, and redirects to the associated long url page.
+//GET CLICKABLE SHORT URL page (redirect to long url)  
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-//prints hello in browser
+//GET hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//prints urlDatabase in json format in the browser
+//GET URLDATABASE info
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//inline html string sent that the browser will render upon receiving this response
+//GET hello, in-line html
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-//allows client server interaction on specified port 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+
+
+
+
+//POST REQUESTS
+
+//GENERATE TINY URL
+app.post("/urls", (req, res) => {
+  const newurl = generateRandomString();
+  urlDatabase[newurl] = req.body.longURL;
+  res.redirect(`/urls/${newurl}`);
 });
+
+//DELETE URLS in database and req.params
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const key = req.params.shortURL;
+  delete urlDatabase[key];
+  delete req.params.shortURL;
+  res.redirect("/urls");
+});
+
+//UPDATE A LONG URL
+app.post("/urls/:shortURL", (req, res) => {
+  const key = req.params.shortURL;
+  urlDatabase[key] = req.body.longURL;
+  res.redirect(`/urls/`);
+});
+
