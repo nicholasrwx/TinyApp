4b667fb61afb0235d402992e8200ab709f1332ac
Nicholas Wiebe <digitalexistence18@gmail.com>
2021-01-22 18:15:19 -0700

    Made URLs and Permissions User Specific

--- express_server.js
+++ express_server.js
@@ -28,12 +28,14 @@ let loggedinUser;
 
 //urlDatabase
 const urlDatabase = {
-  b2xVn2: "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+  b2xVn2: { longURL: "http://www.lighthouselabs.ca", UserID: "admin" },
+  "9sm5xK": { longURL: "http://www.google.com", UserID: "admin" },
 };
 
 //USERS DataStore
-const users = { };
+const users = {
+  admin: { id: "admin", email: "admin@tinyapp.com", password: "admin" },
+};
 
 //VALIDATOR for username and password
 const validator = function (reqEmail, reqPassword, usr) {
@@ -58,38 +60,65 @@ const validator = function (reqEmail, reqPassword, usr) {
   return null;
 };
 
-
-const EmailError = function(reqEmail) {
-
-
+const EmailError = function (reqEmail) {
   for (let user in users) {
-if (users[user].email === reqEmail) { return true} 
-} 
-return false;
-}
+    if (users[user].email === reqEmail) {
+      return true;
+    }
+  }
+  return false;
+};
 
-const PassError = function(reqPassword) {
-for (let user in users) {
-if (users[user].password === reqPassword) { return true};
-}
-return false;
-}
+const PassError = function (reqPassword) {
+  for (let user in users) {
+    if (users[user].password === reqPassword) {
+      return true;
+    }
+  }
+  return false;
+};
 
+const urlsForUser = function (id, urlDatabase) {
+  let shortList = {};
 
+  for (let short in urlDatabase) {
+    console.log(id);
+    console.log(urlDatabase[short]["UserID"]);
+    if (urlDatabase[short]["UserID"] === id) {
+      shortList[short] = urlDatabase[short]["longURL"];
+    }
+  }
 
+  if (Object.entries(shortList).length === 0) {
+    console.log(false);
+    return false;
+  } else {
+    console.log(shortList);
+    return shortList;
+  }
+};
 
 //GET REQUESTS
 
 //GET URLS INDEX page
+
+app.get("/", (req, res) => {
+  const templateVars = { urls: urlDatabase, username: loggedinUser };
+  res.render("homepage", templateVars);
+});
+
 app.get("/urls", (req, res) => {
-  console.log(req.cookies["user_id"]);
-  console.log(users);
   if (!req.cookies["user_id"]) {
     loggedinUser = null;
   } else {
     loggedinUser = users[req.cookies["user_id"]];
   }
-  const templateVars = { urls: urlDatabase, username: loggedinUser };
+
+  const templateVars = {
+    urls: urlsForUser(req.cookies["user_id"], urlDatabase),
+    username: loggedinUser,
+    error: null,
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -107,8 +136,12 @@ app.get("/login", (req, res) => {
 
 //GET NEW URL page
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: loggedinUser };
-  res.render("urls_new", templateVars);
+  if (loggedinUser !== null && loggedinUser !== undefined) {
+    const templateVars = { urls: urlDatabase, username: loggedinUser };
+    res.render("urls_new", templateVars);
+  }
+
+  res.redirect(`/login`);
 });
 
 //GET URL SHOW page
@@ -117,16 +150,17 @@ app.get("/urls/:shortURL", (req, res) => {
   const key = req.params.shortURL;
   const templateVars = {
     shortURL: key,
-    longURL: urlDatabase[key],
+    longURL: urlDatabase[key]["longURL"],
     urls: u,
     username: loggedinUser,
+    error: null,
   }; //urlDatabase?u
   res.render("urls_show", templateVars);
 });
 
 //GET CLICKABLE SHORT URL page (redirect to long url)
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL]["longURL"];
   res.redirect(longURL);
 });
 
@@ -139,47 +173,46 @@ app.get("/*", (req, res) => {
 
 //LOGIN
 app.post("/login", (req, res) => {
-  console.log(req.body);
-
   let reqEmail = req.body.email;
   let reqPassword = req.body.password;
   let data = validator(reqEmail, reqPassword);
-  console.log(data);
   let usr = data;
-  
-  console.log(EmailError(reqEmail) === false);
-  console.log(PassError(reqPassword) === false);
 
-if (EmailError(reqEmail) === false && PassError(reqPassword) === true) {
-  res.status(403);
-  const templateVars = {
-    urls: urlDatabase,
-    username: null,
-    error: "403 Error - Incorrect E-mail!",
-  };
-  res.render("login", templateVars);
-} else if (PassError(reqPassword) === false && EmailError(reqEmail) === true) {
-  res.status(403);
-  const templateVars = {
-    urls: urlDatabase,
-    username: null,
-    error: "403 Error - Incorrect Password!",
-  };
-  res.render("login", templateVars);
-} else if (EmailError(reqEmail) === false && PassError(reqPassword) === false) {
-  res.status(403);
-  const templateVars = {
-    urls: urlDatabase,
-    username: null,
-    error: "403 Error - E-mail and Password are Incorrect, Please Try Again!",
-  };
-  res.render("login", templateVars);
-} else if (usr === true) {
+  if (EmailError(reqEmail) === false && PassError(reqPassword) === true) {
+    res.status(403);
+    const templateVars = {
+      urls: urlDatabase,
+      username: null,
+      error: "403 Error - Incorrect E-mail!",
+    };
+    res.render("login", templateVars);
+  } else if (
+    PassError(reqPassword) === false &&
+    EmailError(reqEmail) === true
+  ) {
+    res.status(403);
+    const templateVars = {
+      urls: urlDatabase,
+      username: null,
+      error: "403 Error - Incorrect Password!",
+    };
+    res.render("login", templateVars);
+  } else if (
+    EmailError(reqEmail) === false &&
+    PassError(reqPassword) === false
+  ) {
+    res.status(403);
+    const templateVars = {
+      urls: urlDatabase,
+      username: null,
+      error: "403 Error - E-mail and Password are Incorrect, Please Try Again!",
+    };
+    res.render("login", templateVars);
+  } else if (usr === true) {
     usr = validator(reqEmail, reqPassword, usr);
     res.cookie("user_id", usr);
     res.redirect("/urls");
   } else {
-    console.log(users);
     res.redirect("/register");
     console.log(`user doesn't exist`);
   }
@@ -187,10 +220,8 @@ if (EmailError(reqEmail) === false && PassError(reqPassword) === true) {
 
 //LOGOUT
 app.post("/logout", (req, res) => {
-  console.log(req.body.name);
   //delete req.cookies["username"];
   res.clearCookie("user_id", req.body.name);
-  console.log(req.body.name);
   res.redirect("/urls");
 });
 
@@ -229,24 +260,63 @@ app.post("/register", (req, res) => {
   }
 });
 
-//GENERATE TINY URL
+//GENERATE TINY URL --------------------------------------------
 app.post("/urls", (req, res) => {
   const newurl = generateRandomString();
-  urlDatabase[newurl] = req.body.longURL;
+  urlDatabase[newurl] = {
+    longURL: req.body.longURL,
+    UserID: req.cookies["user_id"],
+  };
   res.redirect(`/urls/${newurl}`);
+
+  // urlDatabase[]
 });
 
 //DELETE URLS in database and req.params
 app.post("/urls/:shortURL/delete", (req, res) => {
   const key = req.params.shortURL;
-  delete urlDatabase[key];
-  delete req.params.shortURL;
-  res.redirect("/urls");
+  const u = "urls";
+  const templateVars = {
+    shortURL: key,
+    longURL: urlDatabase[key]["longURL"],
+    urls: u,
+    username: loggedinUser,
+    error: "Permission Denied",
+  };
+
+  if (req.cookies["user_id"] === urlDatabase[key]["UserID"]) {
+    const key = req.params.shortURL;
+    delete urlDatabase[key];
+    delete req.params.shortURL;
+    res.redirect("/urls");
+  }
+
+  res.status(403);
+  res.render(`urls_index`, templateVars);
 });
 
-//UPDATE A LONG URL
+//UPDATE A LONG URL -------------------------------------------
 app.post("/urls/:shortURL", (req, res) => {
   const key = req.params.shortURL;
-  urlDatabase[key] = req.body.longURL;
-  res.redirect(`/urls`);
+  const u = "urls";
+  const templateVars = {
+    shortURL: key,
+    longURL: urlDatabase[key]["longURL"],
+    urls: u,
+    username: loggedinUser,
+    error: "Permission Denied",
+  };
+  console.log(key);
+  console.log(req.cookies["user_id"]);
+  console.log(urlDatabase[key["UserID"]]);
+  //is there a user logged in?
+  if (req.cookies["user_id"] === urlDatabase[key]["UserID"]) {
+    urlDatabase[key]["longURL"] = req.body.longURL;
+    res.redirect(`/urls`);
+  }
+
+  res.status(403);
+  res.render(`urls_show`, templateVars);
+
+  //else error -> stay on same page
 });
